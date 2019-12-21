8d2de988ac7c541dfc86914eb42944fa71e66bdc
Loïc <labede.loic@gmail.com>
2019-12-21 14:52:31 +0100

    add(projet): exercices classes 3
    Wish you a merry christmas and a happy new year

---
+++ Classes3/Article.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes3
+{
+    public class Article
+    {
+        private string _reference;
+        private string _designation;
+        private double _prixHT;
+
+        public string Reference
+        {
+            get { return _reference; }
+            set { _reference = value; }
+        }
+
+        public string Designation
+        {
+            get { return _designation; }
+            set { _designation = value; }
+        }
+
+        public double PrixHT
+        {
+            get { return _prixHT; }
+            set { _prixHT = value; }
+        }
+
+
+        public Article() { }
+
+        public Article(string reference, string designation, double prixHT)
+        {
+            this.Reference = reference;
+            this.Designation = designation;
+            this.PrixHT = prixHT;
+        }
+
+        public Article(string reference, string designation)
+        {
+            this.Reference = reference;
+            this.Designation = designation;
+        }
+
+        public Article(Article articleToCopy)
+        {
+            this.Reference = articleToCopy.Reference;
+            this.Designation = articleToCopy.Designation;
+            this.PrixHT = articleToCopy.PrixHT;
+        }
+
+
+        public double CalculerPrixTTC()
+        {
+            double prixTTC = this.PrixHT + (this.PrixHT * Program.TauxTVA / 100);
+            return prixTTC;
+        }
+
+
+        public void AfficherArticle()
+        {
+            Console.WriteLine("=-=-=-=-=-=-=-= Article =-=-=-=-=-=-=-=");
+            Console.WriteLine("Référence : " + this.Reference);
+            Console.WriteLine("Nom : " + this.Designation);
+            Console.WriteLine("Prix HT : " + this.PrixHT);
+            Console.WriteLine("TVA : " + Program.TauxTVA);
+            Console.WriteLine("Prix TTC : " + this.CalculerPrixTTC());
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+        }
+    }
+}
---
+++ Classes3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes3
+{
+    class Program
+    {
+        private const int _tauxTVA = 20;
+        public static int TauxTVA
+        {
+            get { return _tauxTVA; }
+        }
+
+        static void Main(string[] args)
+        {
+            Article article1 = new Article();
+            Article article2 = new Article("RF002", "cadeau");
+            Article article3 = new Article("RF002", "Bûche de noël", 25);
+            Article article4 = new Article("RF003", "Sapin", 80);
+            Article article5 = new Article(article3);
+
+            article1.AfficherArticle();
+            article2.AfficherArticle();
+            article3.AfficherArticle();
+            article4.AfficherArticle();
+            article5.AfficherArticle();
+            Console.ReadLine();
+        }
+    }
+}
